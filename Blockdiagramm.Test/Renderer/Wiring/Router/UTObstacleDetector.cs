18b6a148b6550c15fadd221ac00134f1e96be12b
小野寺つさき <apertureelectronic@outlook.com>
2023-04-15 07:44:09 +0800

    Add unit test project

---
+++ Blockdiagramm.Test/Renderer/Wiring/Router/UTObstacleDetector.cs
@@ -0,0 +1,11 @@
+namespace Blockdiagramm.Test.Renderer.Wiring.Router
+{
+    [TestClass]
+    public class UTObstacleDetector
+    {
+        [TestMethod]
+        public void TestSegmentCrossDetector()
+        {
+        }
+    }
+}
